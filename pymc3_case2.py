df078d68c1b0d0c6a05d9ec169cbe48ef41ff3cd
Bernard Roesler <bernard.roesler@gmail.com>
2019-06-21 11:55:51 -0400

    Add plots to case study 2.

--- pymc3_case2.py
+++ pymc3_case2.py
@@ -32,10 +32,8 @@ ax.plot(years, disaster_data, 'o', markersize=8)
 ax.set(ylabel='Disaster count',
        xlabel='Year')
 
-plt.show()
-
 with pm.Model() as disaster_model:
-    # Switch point when disaster rate reduced
+    # Switch point when disaster rate reduced, ensure test value in range
     s = pm.DiscreteUniform('s', lower=years.min(), upper=years.max(), testval=1900)
 
     # Priors for pre- and post-switch rates number of disasters
@@ -45,11 +43,37 @@ with pm.Model() as disaster_model:
     # Allocate appropriate Poisson rates to years before and after current
     rate = pm.math.switch(years <= s, early_rate, late_rate)
 
+    # Define the number of disasters as a Poisson distribution
     disasters = pm.Poisson('disasters', rate, observed=disaster_data)
 
+# Run the MCMC using Metropolis-Hastings algorithm (discrete variables)
+Ns = 10000
 with disaster_model:
-    trace = pm.sample(10000)
+    trace = pm.sample(Ns)
 
-pm.traceplot(trace)
+# pm.traceplot(trace)  # plot the convergence of parameters
+
+fig = plt.figure(3, figsize=(10,8), clear=True)
+ax = fig.add_subplot(111)
+ax.scatter(years, disaster_data)
+ax.set_xlabel('Year')
+ax.set_ylabel('Number of Accidents')
+
+ax.axvline(trace['s'].mean(), disaster_data.min(), disaster_data.max(), color='C1')
+avg_disasters = np.zeros_like(disaster_data, dtype='float')
+for i, year in enumerate(years):
+    idx = year < trace['s']
+    avg_disasters[i] =  (trace['early_rate'][idx].sum()\
+                       + trace['late_rate'][~idx].sum())\
+                       / (len(trace) * trace.nchains)
+
+# Highest Posterior Density (minimum width Bayesian credible interval)
+sp_hpd = pm.hpd(trace['s'])
+plt.fill_betweenx(x1=sp_hpd[0], x2=sp_hpd[1],
+                  y=[disaster_data.min(), disaster_data.max()],
+                  alpha=0.5, color='C1')
+plt.plot(years, avg_disasters, 'k--', lw=2)
+
+plt.show()
 #==============================================================================
 #==============================================================================
