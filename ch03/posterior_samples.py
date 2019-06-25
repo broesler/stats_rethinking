908849088861a00a48dbd0384953d692b8138555
Bernard Roesler <bernard.roesler@gmail.com>
2019-06-24 23:21:37 -0400

    Create Figure 3.2

--- ch03/posterior_samples.py
+++ ch03/posterior_samples.py
@@ -34,6 +34,9 @@ p_grid, posterior, prior = utils.grid_binom_posterior(Np, k, n,
 Ns = 100_000
 samples = np.random.choice(p_grid, p=posterior, size=Ns, replace=True)
 
+## Exact analytical posterior for comparison
+Beta = stats.beta(k+1, n-k+1)  # Beta(\alpha = 1, \beta = 1) == U(0, 1)
+
 ## Intervals of defined boundaries
 # Sum the grid search posterior
 print(f'P(p < 0.5) = {np.sum(posterior[p_grid < 0.5]):.8f}')
@@ -51,6 +54,7 @@ with np.printoptions(formatter={'float': '{:10.8f}'.format}):
 #------------------------------------------------------------------------------ 
 #        Plot the posterior samples
 #------------------------------------------------------------------------------
+# Figure 3.1
 fig = plt.figure(1, clear=True)
 gs = GridSpec(nrows=1, ncols=2)
 ax1 = fig.add_subplot(gs[1])
@@ -64,6 +68,45 @@ ax2.set(xlabel='Sample number',
         ylabel='$p$')
 
 gs.tight_layout(fig)
+
+# Figure 3.2
+fig = plt.figure(2, clear=True)
+gs = GridSpec(nrows=2, ncols=2)
+axes = list()
+for i in range(2):
+    axes.append(list())
+    for j in range(2):
+        axes[i].append(fig.add_subplot(gs[i,j]))  # left side plot
+        axes[i][j].plot(p_grid, Beta.pdf(p_grid),
+                        c='k', lw=1, label='Beta$(k+1, n-k+1)$')
+        axes[i][j].set(xlabel='$p$',
+                       ylabel='Density')
+
+idx = p_grid < 0.5
+axes[0][0].fill_between(p_grid[idx], Beta.pdf(p_grid[idx]), alpha=0.5)
+axes[0][0].set_title('$p < 0.50$')
+
+idx = np.logical_and(p_grid > 0.5, p_grid < 0.75)
+axes[0][1].fill_between(p_grid[idx], Beta.pdf(p_grid[idx]), alpha=0.5)
+axes[0][1].set_title('$0.50 < p < 0.75$')
+
+idx = p_grid < Beta.ppf(0.8)
+axes[1][0].fill_between(p_grid[idx], Beta.pdf(p_grid[idx]), alpha=0.5)
+axes[1][0].set_title('lower 80%')
+
+idx = np.logical_and(p_grid > Beta.ppf(0.10), p_grid < Beta.ppf(0.9))
+axes[1][1].fill_between(p_grid[idx], Beta.pdf(p_grid[idx]), alpha=0.5)
+axes[1][1].set_title('middle 80%')
+
+gs.tight_layout(fig)
+
+# Plot a highly skewed
+
+
+
+
+
+
 plt.show()
 #==============================================================================
 #==============================================================================
