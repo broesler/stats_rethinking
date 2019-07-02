644166d8f4054fdf4c54204dd24eedf2548223b9
Bernard Roesler <bernard.roesler@gmail.com>
2019-07-01 23:06:02 -0400

    Create solution file for ch03 exercises

---
+++ ch03/ch03_hard.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+#==============================================================================
+#     File: ch03_hard.py
+#  Created: 2019-07-01 22:32
+#   Author: Bernie Roesler
+#
+"""
+  Description: Solutions to Hard Exercises in Chapter 3.
+"""
+#==============================================================================
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+from matplotlib.gridspec import GridSpec
+from scipy import stats
+
+import stats_rethinking as sts
+
+plt.style.use('seaborn-darkgrid')
+np.random.seed(56)  # initialize random number generator
+
+# Load the given data: 1 == 'boy', 2 == 'girl'
+birth1 = np.array([1,0,0,0,1,1,0,1,0,1,0,0,1,1,0,1,1,0,0,0,1,0,0,0,1,0,
+0,0,0,1,1,1,0,1,0,1,1,1,0,1,0,1,1,0,1,0,0,1,1,0,1,0,0,0,0,0,0,0,
+1,1,0,1,0,0,1,0,0,0,1,0,0,1,1,1,1,0,1,0,1,1,1,1,1,0,0,1,0,1,1,0,
+1,0,1,1,1,0,1,1,1,1])
+
+birth2 = np.array([0,1,0,1,0,1,1,1,0,0,1,1,1,1,1,0,0,1,1,1,0,0,1,1,1,0,
+1,1,1,0,1,1,1,0,1,0,0,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,
+1,1,1,0,1,1,0,1,1,0,1,1,1,0,0,0,0,0,0,1,0,0,0,1,1,0,0,1,0,0,1,1,
+0,0,0,1,1,1,0,0,0,0])
+
+df = pd.DataFrame(np.vstack([birth1, birth2]).T, columns=['birth1', 'birth2'])
+
+# Compute the posterior distribution for:
+#   P(boy | data) ∝ P(data | boy) * P(boy)  
+#
+Np = 1000                        # [-] size of parameter grid
+n = df.size                      # trials
+k = np.sum(df.values.flatten())  # boys
+
+# prior: P(p) ~ U(0, 1)
+p_grid, posterior, prior = sts.grid_binom_posterior(Np, k, n)
+
+#==============================================================================
+#==============================================================================
