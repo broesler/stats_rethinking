109cb3916c072c4a2b7c0f3cdb27aff8f229f5c5
Bernard Roesler <bernard.roesler@gmail.com>
2019-07-09 23:01:27 -0400

    Replicate Figure 4.2.

--- ch04/gaussians.py
+++ ch04/gaussians.py
@@ -50,14 +50,40 @@ ax1.plot(players[idx], 'C0-', lw=2)
 ax1.set(xlabel='step number',
         ylabel='position [m]')
 
+
 # Plot distribution of final locations for each of N \in {4, 8, 16}
 axes = list()
 for i, n in enumerate([4, 8, 16]):
-    ax = fig.add_subplot(gs[1, i])
-    axes.append(ax)
-    kde = sts.density(players[:, n])
-    ax.plot(kde)
+    # Plot line in first subplot
+    ax1.axvline(n, c='k', ls='--', lw=1, alpha=0.5)
 
+    # Add secondary subplot
+    ax = fig.add_subplot(gs[1, i])
+    if i > 0:
+        ax.get_shared_x_axes().join(ax, axes[i-1])
+    axes.append(ax)
+
+    data = players[:, n]  # the relevant column
+
+    # Generate the kernel density estimate
+    max_val = np.ceil(np.max(np.abs(data)))
+    x = np.linspace(-max_val, max_val, 1000)
+
+    # sns.distplot(data, ax=ax)
+    kde = sts.density(data, adjust=0.5).pdf(x)
+    ax.plot(x, kde, 'C0')
+
+    # Plot a Gaussian for comparison
+    norm = stats.norm(loc=data.mean(), scale=data.std())
+    ax.plot(x, norm.pdf(x), 'k-', alpha=0.5)
+
+    ax.set_title(f"{n} steps", fontsize=12)
+    ax.set(xlabel='position',
+           ylabel='Density')
+
+ax.set_xlim((max_val-1)*np.array([-1, 1]))
+
+gs.tight_layout(fig)
 plt.show()
 #==============================================================================
 #==============================================================================
