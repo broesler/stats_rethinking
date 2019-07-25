8f9c108c2a2892143611ffcae4f2e27e3edc9a90
Bernard Roesler <bernard.roesler@gmail.com>
2019-07-24 21:55:08 -0400

    Plot a poor joint prior.

--- ch04/howell_model.py
+++ ch04/howell_model.py
@@ -77,12 +77,55 @@ sample_sigma = sigma.rvs(N)
 prior_h = stats.norm(sample_mu, sample_sigma)
 sample_h = prior_h.rvs(N)
 
-plt.figure(3, clear=True)
-ax = sns.distplot(sample_h, fit=stats.norm)
+# Compare to a wider distribution
+sample_mu_wide = stats.norm(178, 100).rvs(N)
+sample_h_wide = stats.norm(sample_mu_wide, sample_sigma).rvs(N)
+
+wadlow_height = 272  # tallest man ever
+
+fig = plt.figure(3, figsize=(10,6), clear=True)
+gs = GridSpec(nrows=1, ncols=2)
+
+# Plot the desired joint prior
+ax = fig.add_subplot(gs[0])
+sns.distplot(sample_h, fit=stats.norm, ax=ax)
 ax.axvline(sample_h.mean(), c='k', ls='--', lw=1)
-ax.set(title='$h \sim \mathcal{N}(\mu, \sigma)$',
+ax.set(xlim=(0, 350),
+       title='$h \sim \mathcal{N}(\mu, \sigma)$',
        xlabel=col, 
        ylabel='density')
+ax.ticklabel_format(axis='y', style='sci', scilimits=(-1, 1))
+
+# Plot a poor joint prior
+ax = fig.add_subplot(gs[1])
+sns.distplot(sample_h_wide, fit=stats.norm, ax=ax)
+ax.axvline(sample_h_wide.mean(), c='k', ls='--', lw=1)
+ax.axvline(0.0, c='k', ls='-.', lw=1)
+ax.axvline(wadlow_height, c='k', ls='-', lw=1)
+ax.set(xlim=(-250, 500),
+        title="$h \sim \mathcal{N}(\mu, \sigma)$\n"\
+              "$\mu \sim \mathcal{N}(178, 100)$",
+       xlabel=col, 
+       ylabel='density')
+ax.ticklabel_format(axis='y', style='sci', scilimits=(-1, 1))
+
+gs.tight_layout(fig)
+
+# Check the percentile of the tallest man in the world
+def print_percentiles(test_height):
+    top_h = stats.percentileofscore(sample_h, test_height)
+    top_h_wide = stats.percentileofscore(sample_h_wide, test_height)
+    print(f"---------- Percentile of {test_height:.1f} ----------")
+    print(f"         N(mu, sigma): {top_h:.2f}")
+    print(f"N(N(178, 100), sigma): {top_h_wide:.2f}")
+
+print_percentiles(0.0)
+print_percentiles(wadlow_height)
+
+#------------------------------------------------------------------------------ 
+#        Grid approximation of the posterior
+#------------------------------------------------------------------------------
+# P(h | data) ∝ P(data | h) * P(h) = P(data | h) * N(N(m, s), U(0, v))
 
 #==============================================================================
 #==============================================================================
