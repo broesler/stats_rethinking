4794e6a8f9f4ba8f556789e2183ed529aef2cdeb
Bernard Roesler <bernard.roesler@gmail.com>
2022-08-19 14:16:59 -0400

    Update tutorial to use numpy rng.

--- pymc_tutorial.py
+++ pymc_tutorial.py
@@ -19,7 +19,7 @@ import pymc as pm
 from matplotlib import cm
 
 plt.style.use('seaborn-darkgrid')
-np.random.seed(123)  # initialize random number generator
+rng = np.random.default_rng(123)  # initialize random number generator
 
 N = 100  # size of dataset
 
@@ -29,18 +29,18 @@ beta = np.array([[1, 1, 2.5]]).T  # (p+1,1) include "alpha" intercept in beta
 
 # Predictor variable (N, p+1)
 X = np.hstack([np.ones((N, 1)),  # add column of ones for intercept
-               np.random.randn(N, 1),
-               np.random.randn(N, 1) * 0.2])
+               rng.standard_normal((N, 1)),
+               rng.standard_normal((N, 1)) * 0.2])
 
 p = X.shape[1] - 1  # number of features
 
 # Simulate outcome variable (model values + additional noise)
-Y = X @ beta + np.random.randn(N, 1)*sigma
+Y = X @ beta + rng.standard_normal((N, 1))*sigma
 
 df = pd.DataFrame(data=np.hstack([X, Y]), columns=['X0', 'X1', 'X2', 'Y'])
 
 # Plot data vs each predictor axis
-fig, ax = plt.subplots(2, 1, num=1, sharex=True, sharey=True, 
+fig, ax = plt.subplots(2, 1, num=1, sharex=True, sharey=True,
                        figsize=(5, 8), clear=True, constrained_layout=True)
 for i in range(len(ax)):
     ax[i].scatter(X[:, i+1], Y)
@@ -82,9 +82,12 @@ with basic_model:
     pm.sample()  # initialize NUTS
     map_estimate = pm.find_MAP(model=basic_model)
 
-print("MAP Estimate:\n-------------\nbeta_h = {}\nsigma_h = {}\n"
-      .format(map_estimate['beta_h'].squeeze(),
-              map_estimate['sigma_h']))
+print("True Params:\n-------------\n"
+      f"beta_h = {beta.squeeze()}\n"
+      f"sigma_h = {sigma}\n\n"
+      "MAP Estimate:\n-------------\n"
+      f"beta_h = {map_estimate['beta_h'].squeeze()}\n"
+      f"sigma_h = {map_estimate['sigma_h']}")
 
 # Use MCMC estimation
 with basic_model:
