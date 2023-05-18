Fix bug in hpdi that return wrong shape. Now same as percentiles.

ch04/4H1.py
@@ -30,7 +30,7 @@ df = pd.read_csv(data_path + 'Howell1.csv')
 # ----------------------------------------------------------------------------- 
 #         4H1. Unknown weights
 # -----------------------------------------------------------------------------
-UNK_W = np.c_[[46.95, 43.72, 64.78, 32.59, 54.63]]
+UNK_W = np.r_[46.95, 43.72, 64.78, 32.59, 54.63]
 
 # Plot the raw data, separate adults and children
 is_adult = df['age'] >= 18
@@ -67,13 +67,13 @@ with pm.Model() as the_model:
     quap = sts.quap()
     post = quap.sample()
 
-mu_samp = post['alpha'].values + post['beta'].values * (UNK_W - w_bar)
+mu_samp = post['alpha'].values + post['beta'].values * (np.c_[UNK_W] - w_bar)
 h_samp = stats.norm(mu_samp, post['sigma']).rvs()
 h_mean = h_samp.mean(axis=1)
-h_hpdi = sts.hpdi(h_samp.T, q=0.89)
+h_hpdi = sts.hpdi(h_samp, q=0.89, axis=1)  # (2, ...)
 
 with pd.option_context('display.float_format', '{:.2f}'.format):
-    print(pd.DataFrame(np.c_[UNK_W, h_mean, h_hpdi], 
+    print(pd.DataFrame(np.c_[UNK_W, h_mean, h_hpdi.T], 
                        columns=['weight', 'height', '|89%', '89%|']))
 
 # Output:
@@ -85,7 +85,7 @@ with pd.option_context('display.float_format', '{:.2f}'.format):
 # 4   54.63   163.38   155.21   171.43
 
 # Plot predictions with original data
-ax.errorbar(UNK_W, h_mean, yerr=np.abs(h_hpdi.T - h_mean),
+ax.errorbar(UNK_W, h_mean, yerr=np.abs(h_hpdi - h_mean),
             ls='-', lw=1, marker='.', markersize=10, c='C3',
             capsize=4, ecolor='k', elinewidth=1, alpha=0.8,
             label='Predicted Data')

ch04/4H2.py
@@ -66,16 +66,16 @@ sts.precis(quap)
 # there is an 27.2 cm increase in height.
 
 q = 0.89
-w = np.c_[np.linspace(0, 1.05*weight.max(), 20)]
-mu_samp = post['alpha'].values + post['beta'].values * (w - w_bar)
+w = np.linspace(0, 1.05*weight.max(), 20)
+mu_samp = post['alpha'].values + post['beta'].values * (np.c_[w] - w_bar)
 mu_mean = mu_samp.mean(axis=1)
 h_samp = stats.norm(mu_samp, post['sigma']).rvs()
 h_mean = h_samp.mean(axis=1)
-h_hpdi = sts.hpdi(h_samp.T, q=q)
+h_hpdi = sts.hpdi(h_samp, q=q, axis=1)
 
 # Plot predictions with original data
 ax.plot(w, mu_mean, 'k', label='MAP Estimate')
-ax.fill_between(w[:, 0], h_hpdi[:, 0], h_hpdi[:, 1],
+ax.fill_between(w, h_hpdi[0], h_hpdi[1],
                 facecolor='k', alpha=0.2, interpolate=True,
                 label=f"{100*q:g}% CI")
 ax.legend()

ch04/4H3.py
@@ -64,22 +64,22 @@ with pm.Model() as the_model:
 sts.precis(quap)
 
 q = 0.97
-w = np.c_[np.logspace(0, np.log10(1.05*weight.max()), 20)]
+w = np.logspace(0, np.log10(1.05*weight.max()), 20)
 
-mu_samp = post['alpha'].values + post['beta'].values * np.log(w)
+mu_samp = post['alpha'].values + post['beta'].values * np.log(np.c_[w])
 mu_mean = mu_samp.mean(axis=1)
-mu_hpdi = sts.hpdi(mu_samp.T, q=q)
+mu_hpdi = sts.hpdi(mu_samp, q=q, axis=1)
 
 h_samp = stats.norm(mu_samp, post['sigma']).rvs()
 h_mean = h_samp.mean(axis=1)
-h_hpdi = sts.hpdi(h_samp.T, q=q)
+h_hpdi = sts.hpdi(h_samp, q=q, axis=1)
 
 # Plot predictions with original data
 ax.plot(w, mu_mean, 'k', label='MAP Estimate')
-ax.fill_between(w[:, 0], mu_hpdi[:, 0], mu_hpdi[:, 1],
+ax.fill_between(w, mu_hpdi[0], mu_hpdi[1],
                 facecolor='k', alpha=0.3, interpolate=True,
                 label=f"{100*q:g}% CI")
-ax.fill_between(w[:, 0], h_hpdi[:, 0], h_hpdi[:, 1],
+ax.fill_between(w, h_hpdi[0], h_hpdi[1],
                 facecolor='k', alpha=0.2, interpolate=True,
                 label=f"{100*q:g}% CI")
 ax.legend()

stats_rethinking/utils.py
@@ -196,7 +196,8 @@ def hpdi(data, q=0.89, verbose=False, width=6, precision=4,
         Q = az.hdi(A, hdi_prob=qs, **kwargs)
     else:
         Q = np.stack([az.hdi(A, hdi_prob=q, **kwargs) for q in qs])
-        Q = np.moveaxis(Q, -1, 0)  # (Q, M, P, 2) -> (2, Q, M, P)
+
+    Q = np.moveaxis(Q, -1, 0)  # (Q, M, P, 2) -> (2, Q, M, P)
 
     if verbose:
         fstr = f"{width}.{precision}f"