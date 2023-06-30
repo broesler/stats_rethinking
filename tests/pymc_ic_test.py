100a78bd3b1d8b7fcbed18c510d428d6cfbbea16
Bernard Roesler <bernard.roesler@gmail.com>
2023-06-30 15:05:27 -0400

    Reorganize. Compute ICs using built-in methods.

--- tests/pymc_ic_test.py
+++ tests/pymc_ic_test.py
@@ -14,20 +14,61 @@ import arviz as az
 import numpy as np
 import pandas as pd
 import pymc as pm
-# import xarray as xr
 
 from scipy import stats
 
 import stats_rethinking as sts
 
-Y_SIGMA = 1
+# Function arguments
 N = 20
 k = 3
 rho = np.r_[0.15, -0.4]
 b_sigma = 0.5
 
+# Internal model parameters
+Y_SIGMA = 1
+
+
+def frame_to_dataset(df):
+    """Convert DataFrame to ArviZ DataSet by combinining columns with
+    multi-dimensional parameters, e.g. β__0, β__1, ..., β__N into β (N,).
+    """
+    tf = df.copy()
+    var_names = tf.columns.str.replace('__[0-9]+', '', regex=True).unique()
+    the_dict = dict()
+    for v in var_names:
+        the_dict[v] = np.expand_dims(tf.filter(like=v).values, 0)
+    return az.convert_to_dataset(the_dict)
+
+
+def dataset_to_frame(ds):
+    """Convert ArviZ DataSet to DataFrame by separating columns with
+    multi-dimensional parameters, e.g. β (N,) into β__0, β__1, ..., β__N.
+    """
+    df = pd.DataFrame()
+    for vname, var in ds.data_vars.items():
+        v = var.mean('chain').squeeze()  # remove chain dimension
+        if v.ndim == 1:                  # only draw dimension
+            df[vname] = v.values
+        elif v.ndim > 1:
+            df[_names_from_vec(vname, v.shape[1])] = v.values
+        else:
+            raise ValueError(f"{vname} has invalid dimension {v.ndim}.")
+    return df
+
+
+def _names_from_vec(vname, ncols):
+    """Create a list of strings ['x__0', 'x__1', ..., 'x__``ncols``'],
+    where 'x' is ``vname``."""
+    # TODO case of 2D, etc. variables
+    fmt = '02d' if ncols > 10 else 'd'
+    return [f"{vname}__{i:{fmt}}" for i in range(ncols)]
+
 
 # TODO refactor these functions into generics
+
+# In order to use `pm.compute_log_likelihood` we need to have ind/obs variables
+# in the model itself, and set them to be mm_test/y_test.
 
 def loglik(m, data_in, data_out, Ns=1000):
     """Compute the log-likelihood of the data, given the model."""
@@ -95,82 +136,21 @@ if k > 1:
 # Build and fit the model to the training data
 with pm.Model():
     X = pm.MutableData('X', mm_train)
+    obs = pm.MutableData('obs', y_train)
     if k == 1:
         α = pm.Normal('α', 0, b_sigma)
         μ = pm.Deterministic('μ', α)
-        y = pm.Normal('y', μ, Y_SIGMA, observed=y_train)
+        y = pm.Normal('y', μ, Y_SIGMA, observed=obs)
     else:
         α = pm.Normal('α', 0, b_sigma, shape=(1,))
         βn = pm.Normal('βn', 0, b_sigma, shape=(k-1,))
         β = pm.math.concatenate([α, βn])
         μ = pm.Deterministic('μ', pm.math.dot(X, β))
         y = pm.Normal('y', μ, Y_SIGMA,
-                      observed=y_train,
-                      shape=X[:, 0].shape
+                      observed=obs,
+                      shape=obs.shape
                       )
     q = sts.quap()
-
-# Compute the deviance
-dev = pd.Series(index=['train', 'test'], dtype=float)
-dev['train'] = -2 * np.sum(lppd(q, data_in=mm_train, data_out=y_train))
-
-# Compute the lppd with the test data
-mm_test = np.ones((N, 1))
-if k > 1:
-    mm_test = np.c_[mm_test, X_test[:, :k-1]]
-
-dev['test'] = -2 * np.sum(lppd(q, data_in=mm_test, data_out=y_test))
-
-# Compute WAIC, LOOIC, and LOOCV
-wx = WAIC(q, data_in=mm_test, data_out=y_test)
-waic_s = pd.Series({'test': wx['waic'],
-                    'err': np.abs(wx['waic'] - dev['test'])})
-
-# -----------------------------------------------------------------------------
-#         Test LOOIC computation
-# -----------------------------------------------------------------------------
-Ns = 1000
-y_loglik = loglik(q, mm_test, y_test, Ns)              # (N, Ns)
-y_lppd = sts.logsumexp(y_loglik, axis=1) - np.log(Ns)  # (N,)
-
-post = q.sample(1000)  # DataFrame with columns = ['α', 'βn__0', 'βn__1', ...]
-
-
-def frame_to_array(df):
-    """Convert DataFrame to ArviZ DataSet by combinining columns with
-    multi-dimensional parameters, e.g. β__0, β__1, ..., β__N into β (N,).
-    """
-    tf = df.copy()
-    var_names = tf.columns.str.replace('__[0-9]+', '', regex=True).unique()
-    the_dict = dict()
-    for v in var_names:
-        the_dict[v] = np.expand_dims(tf.filter(like=v).values, 0)
-    return az.convert_to_dataset(the_dict)
-
-
-def array_to_frame(ds):
-    """Convert ArviZ DataSet to DataFrame by separating columns with
-    multi-dimensional parameters, e.g. β (N,) into β__0, β__1, ..., β__N.
-    """
-    df = pd.DataFrame()
-    for vname, var in ds.data_vars.items():
-        v = var.mean('chain').squeeze()  # remove chain dimension
-        if v.ndim == 1:                  # only draw dimension
-            df[vname] = v.values
-        elif v.ndim > 1:
-            df[_names_from_vec(vname, v.shape[1])] = v.values
-        else:
-            raise ValueError(f"{vname} has invalid dimension {v.ndim}.")
-    return df
-
-
-def _names_from_vec(vname, ncols):
-    """Create a list of strings ['x__0', 'x__1', ..., 'x__``ncols``'],
-    where 'x' is ``vname``."""
-    # TODO case of 2D, etc. variables
-    fmt = '02d' if ncols > 10 else 'd'
-    return [f"{vname}__{i:{fmt}}" for i in range(ncols)]
-
 
 # Convert posterior to az.InferenceData for use in pymc log_likelihood
 # computation.
@@ -181,23 +161,113 @@ def _names_from_vec(vname, ncols):
 #            on_unused_input="ignore",
 #        )
 
-da = frame_to_array(post)
-df = array_to_frame(da)
-idata = az.convert_to_inference_data(da)
-idata = pm.compute_log_likelihood(idata, model=q.model, progressbar=False)
+# TODO Try this?
+# elemwise_lppd_fn = model.compile_fn(
+#            inputs=model.value_vars,
+#            outs=model.logp(vars=observed_vars, sum=False),
+#            on_unused_input="ignore",
+#        )
 
-loo = az.loo(idata)
-print(loo)
-loo_dev = -2 * loo.elpd_loo
-loo_s = pd.Series({'test': loo_dev,
-                   'err': np.abs(loo_dev - dev['test'])})
+# Compute the log likelihood
+Ns = 1000
+post = q.sample(Ns)  # DataFrame with columns = ['α', 'βn__0', 'βn__1', ...]
+da = frame_to_dataset(post)
 
-res = pd.concat([dev, waic_s, loo_s], keys=['deviance', 'WAIC', 'LOOIC'])
+# post = dataset_to_frame(da)  # test inverse function
+
+id_train = pm.compute_log_likelihood(
+    idata=az.convert_to_inference_data(da),
+    model=q.model,
+    progressbar=False,
+)
+
+# Compute the deviance
+dev = pd.Series(index=['train', 'test'], dtype=float)
+
+loglik_train = id_train.log_likelihood['y'].mean('chain').values
+lppd_train = (sts.logsumexp(loglik_train, axis=0)
+              - np.log(Ns))
+dev['train'] = -2 * np.sum(lppd_train)
+
+# Compute the lppd with the test data
+mm_test = np.ones((N, 1))
+if k > 1:
+    mm_test = np.c_[mm_test, X_test[:, :k-1]]
+
+# Re-compute the log-likelihood with the new data
+q.model.set_data('X', mm_test)
+q.model.set_data('obs', y_test)
+
+id_test = pm.compute_log_likelihood(
+    idata=az.convert_to_inference_data(da),
+    model=q.model,
+    progressbar=False,
+)
+
+
+loglik_test = id_test.log_likelihood['y'].mean('chain').values
+lppd_test = sts.logsumexp(loglik_test, axis=0) - np.log(Ns)
+dev['test'] = -2 * np.sum(lppd_test)
+
+# NOTE how to convert these into functions and not have to recompute the
+# log-likelihood each time?
+#
+# Solution: multiple dispatch:
+#
+# rethinking::WAIC and rethinking::LOO/PSIS take:
+#   * quap and re-computes log-likelihood, lppd, and penalty term
+#   * list[['log-likelihood',]] and computes lppd and penalty term
+#
+# rethinking::cv_quap takes quap_model only
+#
+# rethinking::LOO/PSIS uses outside library, others computes internally
+
+
+# -----------------------------------------------------------------------------
+#         Compute WAIC
+# -----------------------------------------------------------------------------
+# wx = WAIC(q, data_in=mm_test, data_out=y_test)
+penalty = np.var(loglik_test, axis=0)
+waic_vec = -2 * (lppd_test - penalty)
+n_cases = loglik_test.shape[1]
+std_err = (n_cases * np.var(waic_vec))**0.5
+# w = waic_vec if pointwise else waic_vec.sum()
+# p = penalty if pointwise else penalty.sum()
+w = waic_vec.sum()
+p = penalty.sum()
+wx = dict(waic=w, lppd=lppd_test, penalty=p, std=std_err)
+waic_s = pd.Series({'test': wx['waic'],
+                    'err': np.abs(wx['waic'] - dev['test'])})
+
+# -----------------------------------------------------------------------------
+#         Compute LOOIC
+# -----------------------------------------------------------------------------
+loo = az.loo(id_test)
+# print(loo)
+
+lx = dict(
+    PSIS=-2*loo.elpd_loo,  # == loo_list$estimates['looic', 'Estimate']
+    lppd=loo.elpd_loo,
+    penalty=loo.p_loo,
+    std_err=2*loo.se,      # == loo_list$estimates['looic', 'SE']
+)
+psis_s = pd.Series({'test': lx['PSIS'],
+                   'err': np.abs(lx['PSIS'] - dev['test'])})
+
+# -----------------------------------------------------------------------------
+#         Compute LOOCV
+# -----------------------------------------------------------------------------
+# See rethinking::cv_quap -> expensive!!
+
+res = pd.concat([dev, waic_s, psis_s], keys=['deviance', 'WAIC', 'LOOIC'])
+# res = pd.concat([dev, waic_s, psis_s, loocv_s],
+#                 keys=['deviance', 'WAIC', 'LOOIC', 'LOOCV'])
+print(res)
 
 # -----------------------------------------------------------------------------
 #         Look at actual pymc/arviz examples
 # -----------------------------------------------------------------------------
-# trace = pm.sample(model=q.model, idata_kwargs={'log_likelihood': True})
+# tr = pm.sample(model=q.model, idata_kwargs={'log_likelihood': True})
 # loo = az.loo(trace)
 # data = az.load_arviz_data('centered_eight')
 
