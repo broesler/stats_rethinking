af40887f9558e3d3c941f2b34c7bf077c33139b3
Bernard Roesler <bernard.roesler@gmail.com>
2023-10-02 18:13:32 -0400

    Build model with and without Seychelles.

--- ch08/8H3.py
+++ ch08/8H3.py
@@ -31,27 +31,40 @@ df['rugged_std'] = df['rugged'] / df['rugged'].max()      # [0, 1]
 df['log_GDP'] = np.log(df['rgdppc_2000'])
 df['log_GDP_std'] = df['log_GDP'] / df['log_GDP'].mean()  # proportion of avg
 
-# Fit the interaction model:
-#   y ~ N(μ, σ)
-#   μ ~ α + β_A A + β_R R + β_AR A R
-#   A: cont_africa, R: rugged
+# Drop Seychelles from a version of the dataset
+df_ns = df.loc[df['country'] != 'Seychelles'].copy()
 
-# TODO fit with and without Seychelles
-# Drop Seychelles from the dataset
-df = df.loc[df['country'] != 'Seychelles']
 
-with pm.Model():
-    R = pm.MutableData('R', df['rugged_std'])
-    A = pm.MutableData('A', df['cont_africa'])  # indicator
-    obs = pm.MutableData('obs', df['log_GDP_std'])
-    α = pm.Normal('α', 1, 0.1, shape=(2,))
-    β = pm.Normal('β', 0, 0.3, shape=(2,))
-    μ = pm.Deterministic('μ', α[A] + β[A]*R)
-    σ = pm.Exponential('σ', 1)
-    y = pm.Normal('y', μ, σ, observed=obs, shape=R.shape)
-    m8H3 = sts.quap(data=df)
+def build_model(data):
+    """Fit the interaction model.
 
-sts.precis(m8H3)
+    .. math::
+        y ~ N(μ, σ)
+        μ ~ α + β_A A + β_R R + β_AR A R
+
+    where A = `cont_africa`, R = `rugged`.
+    """
+    with pm.Model():
+        R = pm.MutableData('R', data['rugged_std'])
+        A = pm.MutableData('A', data['cont_africa'])  # index value
+        obs = pm.MutableData('obs', data['log_GDP_std'])
+        α = pm.Normal('α', 1, 0.1, shape=(2,))
+        β = pm.Normal('β', 0, 0.3, shape=(2,))
+        μ = pm.Deterministic('μ', α[A] + β[A]*R)
+        σ = pm.Exponential('σ', 1)
+        y = pm.Normal('y', μ, σ, observed=obs, shape=R.shape)
+        return sts.quap(data=data)
+
+
+# Fit with and without Seychelles
+q_all = build_model(df)
+q_ns = build_model(df_ns)
+
+print(f"{float(q_all.coef['β'][1] / q_ns.coef['β'][1]) = :.2f}")  # ≈ 2.35
+
+# We now have 4 options: (Non-)?African countries, and with(out)? Seychelles.
+ct = sts.coef_table([q_all, q_ns], ['all', 'no Seychelles'])
+sts.plot_coef_table(ct, fignum=1)
 
 # =============================================================================
 # =============================================================================
