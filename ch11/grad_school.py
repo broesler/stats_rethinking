68f4de78dbbcf9e7d5ed6a0e7f6ead8a4e5801d4
Bernard Roesler <bernard.roesler@gmail.com>
2023-12-12 22:21:56 -0500

    Refactor postcheck into a function.

--- ch11/grad_school.py
+++ ch11/grad_school.py
@@ -37,6 +37,7 @@ df = pd.read_csv(Path('../data/UCBadmit.csv'))
 # dtypes: int64(3), object(2)
 # memory usage: 576.0 bytes
 
+df = df.reset_index().rename({'index': 'case'}, axis='columns')
 df['dept'] = df['dept'].astype('category')
 df['gender'] = df['applicant.gender'].astype('category')
 df = df.drop('applicant.gender', axis='columns')
@@ -64,62 +65,114 @@ sts.precis(xr.Dataset(dict(diff_a=diff_a, diff_p=diff_p)))
 
 df['admit_p'] = df['admit'] / df['applications']
 
-# TODO implement this function
+
+# TODO move into stats_rethinking
+def postcheck(fit, agg_name=None, N=1000, q=0.89, fignum=None):
+    """Plot the discrete observed data and the posterior predictions.
+
+    Parameters
+    ----------
+    fit : :obj:`sts.PostModel'
+        The model to which the data is fitted.
+    agg_name : str, optional
+        The name of the variable over which the data is aggregated.
+    N : int, optional
+        The number of samples to take of the posterior.
+    q : float in [0, 1], optional
+        The quantile of which to compute the interval.
+    fignum : int, optional
+        The Figure number in which to plot.
+
+    Returns
+    -------
+    ax : plt.Axes
+        The axes in which the plot was drawn.
+    """
+    y = fit.model.observed_RVs[0].name
+    post = fit.get_samples(N)
+
+    yv = fit.data[y].copy()
+    xv = np.arange(len(yv))
+
+    pred = sts.lmeval(
+        fit,
+        out=fit.model['p'],
+        dist=post,
+    )
+
+    sims = sts.lmeval(
+        fit,
+        out=fit.model[y],
+        params=fit.model.free_RVs,  # ignore deterministics
+        dist=post,
+    )
+
+    μ = pred.mean('draw')
+
+    a = (1 - q) / 2
+    μ_PI = pred.quantile([a, 1-a], dim='draw')
+    y_PI = sims.quantile([a, 1-a], dim='draw')
+
+    if agg_name is not None:
+        yv /= fit.data[agg_name]
+        y_PI = y_PI.values / fit.data[agg_name].values
+
+    fig = plt.figure(fignum, clear=True)
+    ax = fig.add_subplot()
+
+    # Plot the mean and simulated PIs
+    ax.errorbar(xv, μ, yerr=np.abs(μ_PI - μ), c='k',
+                ls='none', marker='o', mfc='none', mec='k', label='pred')
+    ax.scatter(np.tile(xv, (2, 1)), y_PI, marker='+', c='k', label='y PI')
+    # Plot the data
+    ax.scatter(xv, yv, c='C0', label='data', zorder=10)
+
+    ax.legend()
+    ax.set(xlabel='case',
+        ylabel=y)
+
+    ax.spines[['top', 'right']].set_visible(False)
+    return ax
+
+
+# (R code 11.31)
 # sts.postcheck(m11_7, fignum=1)
+ax = postcheck(m11_7, agg_name='applications', fignum=1)
 
-# def postcheck(fit, x=None, q=0.89, fignum=None):
-fit = m11_7
-
-y = fit.model.observed_RVs[0].name
-
-# xv = fit.data[x] or yv.index
-yv = fit.data[y]
-
-pred = sts.lmeval(
-    m11_7,
-    out=m11_7.model['p'],
-    dist=post,
-)
-
-sims = sts.lmeval(
-    m11_7,
-    out=m11_7.model['admit'],
-    params=m11_7.model.free_RVs,  # ignore deterministics
-    dist=post,
-)
-
-μ = pred.mean('draw')
-
-q = 0.89
-a = (1 - q) / 2
-μ_PI = pred.quantile([a, 1-a], dim='draw')
-y_PI = sims.quantile([a, 1-a], dim='draw')
-
-# if aggregate:
-yv /= df['applications']
-y_PI = y_PI.values / df['applications'].values
-
-fig = plt.figure(1, clear=True)
-ax = fig.add_subplot()
-ax.scatter(yv.index, yv, c='C0', label='data')
-ax.errorbar(yv.index, μ, yerr=np.abs(μ_PI - μ), 
-            ls='none', marker='o', mfc='none', mec='k', label='pred')
-ax.scatter(np.tile(yv.index, (2, 1)), y_PI,
-           marker='+', c='k', label='y PI')
-ax.set(xlabel='case',
-       ylabel='admit')
+# TODO can maybe generalize the labeling and line grouping given up to 2 levels
+# of grouping. 
+# Need to compute the center of each group for the major label and individual
+# point locations for the minor label.
+# Maybe take kwargs "major_group='dept', minor_group='gender'"
 
 # Connect points in each department
 N_depts = len(df['dept'].cat.categories)
-x = 1 + 2*np.arange(N_depts)
-xv = np.r_[[x, x+1]]
+x = 2*np.arange(N_depts)
+xp = np.r_[[x, x+1]]
 y0 = df.loc[x, 'admit'] / df.loc[x, 'applications']
 y1 = df.loc[x+1, 'admit'] / df.loc[x+1, 'applications']
-yv = np.r_[[y0, y1]]
+yp = np.r_[[y0, y1]]
 
-ax.plot(xv, yv, 'C0')
+ax.plot(xp, yp, 'C0')
 
-# TODO label each line with dept
+# Label the cases
+xv = np.arange(len(df))
+xind = xv[:-1:2]
+
+ax.set_xticks(xind + 0.5)
+ax.set_xticklabels(df.loc[xind, 'dept'])
+
+ax.set_xticks(xv, minor=True)
+ax.set_xticklabels(df['gender'], minor=True)
+ax.tick_params(axis='x', which='minor', pad=18)
+
+ax.tick_params(axis='x', which='major',  bottom=False)  # don't draw the ticks
+ax.tick_params(axis='x', which='minor',  bottom=True)
+
+# Lines between each department for clarity
+for x in xind + 1.5:
+    ax.axvline(x, lw=1, c='k')
+
 
 plt.ion()
 plt.show()
