e769c2ca73a185817318f8f34eb938413c873ee4
Bernard Roesler <bernard.roesler@gmail.com>
2024-01-05 18:24:45 -0500

    Implement 2 different models of cats properly.

--- ch11/cats.py
+++ ch11/cats.py
@@ -4,10 +4,21 @@
 #  Created: 2024-01-04 17:41
 #   Author: Bernie Roesler
 #
-"""
+r"""
 §11.3.2 Actual Cats
 
-See video lecture: <https://youtu.be/Zi6N3GLUJmw?si=PNoNFyq99ImJUUn7&t=4588>
+The model is the following:
+
+    .. math::
+    D_i|A_i = 1 \sim \mathrm{Exponential}(\lambda_i)
+    D_i|A_i = 0 \sim \mathrm{Exponential-CCDF}(\lambda_i)
+    \lambda_i = \frac{1}{\mu_i}
+    \log \mu_i = \alpha_{\mathrm{CID}[i]}
+
+where D is the number of days *without* being adopted, A is an indicator of
+adoption, and CID is the color ID: 1 for Black, 0 otherwise.
+
+*See* video lecture: <https://youtu.be/Zi6N3GLUJmw?si=PNoNFyq99ImJUUn7&t=4588>
 """
 # =============================================================================
 
@@ -15,7 +26,6 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pymc as pm
-import xarray as xr
 
 from pathlib import Path
 from scipy import stats
@@ -51,40 +61,142 @@ for col in ['in_event', 'out_event', 'breed', 'color']:
 # Want: probability that cat has *not* yet been adopted
 # Estimand: Are black cats less likely to be adopted than other colors?
 
+# See <https://discourse.pymc.io/t/simple-exponential-survival-function/4961>
+
+# FIXME off by a factor of ~2 in the output D distribution means?
 # Define the model
+
+# junpenglao's model:
 with pm.Model():
     days_to_event = pm.MutableData('days_to_event', df['days_to_event'])
     color_id = pm.MutableData('color_id', (df['color'] == 'Black').astype(int))
     adopted = pm.MutableData('adopted', df['out_event'] == 'Adoption')
-    α = pm.Normal('α', 0, 1, shape=(2,))  # color_id == 0 or 1
-    μ = pm.Deterministic('μ', pm.math.exp(α[color_id]))
-    λ = pm.Deterministic('λ', 1/μ)
-    # Define probabilities of waiting D days without being adopted
-    # D|A = 1 ~ Exponential(λ)       # observed adoptions
-    # D|A = 0 ~ Exponential-CCDF(λ)  # not-yet-adopted
-    # FIXME off by a factor of ~2 in the output D distribution means?
-    # It seems like the model is not actually taking the `not_yet_adopted` into
-    # account properly, so we're only getting 1/2 the number of days
-    obs_adopted = pm.Exponential('obs_adopted', λ, observed=days_to_event)
-    # R: custom(exponential_lccdf(λ):
-    # not_yet_adopted = pm.math.exp(-λ * days_to_event)
-    # not_yet_adopted = pm.Exponential('nya', λ, observed=days_to_event) / λ
-    not_yet_adopted = pm.math.log(obs_adopted / λ)
-    D = pm.Deterministic(
-        'D',
-        pm.math.switch(adopted, obs_adopted, not_yet_adopted),
-    )
-    m11_14 = sts.ulam(data=df)
 
-sts.precis(m11_14)
+    # # Define model parameters with indicator variable (NOT IDEAL)
+    # a = pm.Normal('a', 0, 1)
+    # b = pm.Normal('b', 0, 1)
+    # log_rate = a + b * color_id
+    # obs_adopted = pm.Exponential('obs_adopted', pm.math.exp(log_rate), observed=days_to_event)
+    # # Correct logp for censored data
+    # survival = pm.Potential('survival', pm.math.switch(adopted, 0, -log_rate))
+
+    # Define model parameters (PREFERRED)
+    α = pm.Normal('α', 0, 1, shape=(2,))  # color_id == 0 or 1
+    μ = pm.math.exp(α[color_id])
+    λ = 1/μ
+    obs_adopted = pm.Exponential('obs_adopted', λ, observed=days_to_event)
+    # Correct logp for censored data
+    survival = pm.Potential('survival', pm.math.switch(adopted, 0, α[color_id]))
+
+    m11_14_junpenglao = sts.ulam(data=df)
+
+print('junpenglao:')
+sts.precis(m11_14_junpenglao)
+post = m11_14_junpenglao.get_samples()
+# print(f"D[0] = {np.exp(-post['a']).mean():.2f}")
+# print(f"D[1] = {np.exp(-(post['a'] + post['b'])).mean():.2f}")
+post['D'] = np.exp(post['α'])
+sts.precis(post)
+
+# Test sampling from the posterior predictive
+postp_junpenglao = pm.sample_posterior_predictive(
+    trace=m11_14_junpenglao.get_samples(),
+    model=m11_14_junpenglao.model,
+    random_seed=56,
+)
+
+
+# john_c's model
+with pm.Model():
+    days_to_event = pm.ConstantData('days_to_event', df['days_to_event'])
+    color_id = pm.ConstantData('color_id', (df['color'] == 'Black').astype(int))
+    adopted = pm.ConstantData('adopted', df['out_event'] == 'Adoption')
+
+    # Define model parameters
+    α = pm.Normal('α', 0, 1, shape=(2,))  # color_id == 0 or 1
+    μ = pm.math.exp(α[color_id])
+    λ = 1/μ
+
+    # obs_adopted = pm.Exponential('obs_adopted', λ, observed=days_to_event)
+
+    # Censored data == not adopted
+    # Example directly from:
+    # <https://www.pymc.io/projects/docs/en/latest/guides/Probability_Distributions.html#custom-distributions>
+    def logp(obs, t, λ):
+        r"""The log probability function.
+
+        Corresponds to a probability function:
+
+        .. math::
+            p(t|\lambda, \mathrm{obs}) = \prod_i \lambda_i^{\mathrm{obs}_i} e^{\lambda_i t_i}
+
+        Parameters
+        ----------
+        obs : PyTensor
+            The observed variable. obs = 0 if data is "censored", 1 otherwise.
+        t : PyTensor
+            The length of time (days in this case) until the event.
+        λ : PyTensor
+            The rate parameter.
+
+        Returns
+        -------
+        result : PyTensor
+            The log probability logp(t | λ, obs).
+        """
+        return (obs * pm.math.log(λ) - λ * t).sum()
+
+    def random(*dist_params, rng=None, size=None):
+        """The function from which to generate random samples.
+
+        Parameters
+        ----------
+        dist_params : list
+            The same parameters as given to `logp`. (t, λ) in this case.
+        rng : Generator
+            The random number generator used internally.
+        size : tuple
+            The desired size of the random draw.
+
+        Returns
+        -------
+        result : (M, N) ndarray
+            Matrix of M vectors in K dimensions
+        """
+        t, λ = dist_params
+        return np.where(
+            np.array(adopted == 1),
+            stats.expon.rvs(scale=1/λ, size=size),                    # not censored = Exp(λ)
+            stats.expon.isf(stats.uniform.rvs(size=size), scale=1/λ)  # censored = Exp(λ).isf()
+        )
+
+    # NOTE DensityDist -> CustomDist in new API
+    exp_surv = pm.CustomDist('exp_surv', days_to_event, λ,
+                             logp=logp, observed=adopted,
+                             random=random)
+
+    m11_14_john = sts.ulam(data=df)
+
+print('john_c:')
+sts.precis(m11_14_john)
+
+# Temp for plots:
+m11_14 = m11_14_junpenglao
+# sts.precis(m11_14)
 
 # Compute average time to adoption
 post = m11_14.get_samples()
 post['D'] = np.exp(post['α'])
 sts.precis(post)
 
+# TODO make plots of the posterior predictives to compare them
+postp_john = pm.sample_posterior_predictive(
+    trace=m11_14_john.get_samples(),
+    model=m11_14_john.model,
+    random_seed=56,
+)
 
-# ----------------------------------------------------------------------------- 
+# -----------------------------------------------------------------------------
 #       Plots
 # -----------------------------------------------------------------------------
 
@@ -93,9 +205,9 @@ fig = plt.figure(1, clear=True)
 ax = fig.add_subplot()
 for idx, label, c in zip([0, 1], ['Other cats', 'Black cats'], ['C3', 'k']):
     # TODO move to function:
-    # sts.plot_density(post['D'].sel(α_dim_0=idx), ax=ax, c=c, label=label) 
+    # sts.plot_density(post['D'].sel(α_dim_0=idx), ax=ax, c=c, label=label)
     x = np.sort(post['D'].sel(α_dim_0=idx).stack(sample=('chain', 'draw')))
-    dens = sts.density(x, adjust=0.5).pdf(x)
+    dens = sts.density(x).pdf(x)
     ax.plot(x, dens, c=c, label=label)
 
 ax.legend()
