15669f8a630b04548791657d83c30105e4246653
Bernard Roesler <bernard.roesler@gmail.com>
2024-01-11 18:04:26 -0500

    Initial model of eagles.

---
+++ ch11/11H3.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# =============================================================================
+#     File: 11H3.py
+#  Created: 2024-01-11 17:51
+#   Author: Bernie Roesler
+#
+r"""
+Solution to 11H3. Bald Eagles data.
+
+.. note:: The problem is incorrectly labeled in the digital book as 10H3.
+
+The model will be:
+
+.. math::
+    \begin{align}
+        y &\sim \mathrm{Binomial}(n, p) \\
+        \log \frac{p}{1 - p} &= \alpha + \beta_P P + \beta_V V + \beta_A A \\
+        \alpha \sim \mathcal{N}(0, 10) \\
+        \beta_P \sim \mathcal{N}(0, 5) \\
+        \beta_V \sim \mathcal{N}(0, 5) \\
+        \beta_A \sim \mathcal{N}(0, 5) \\
+    \end{align}
+
+where
+:math:`y` is the number of successful attempts,
+:math:`n` is the total number of attemps,
+:math:`P` indicates whether the pirate had a large body size,
+:math:`V` indicates whether the victim had a large body size, and
+:math:`A` indicates whether the pirate was an adult.
+"""
+# =============================================================================
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import pymc as pm
+
+from pathlib import Path
+from scipy import stats
+
+import stats_rethinking as sts
+
+df = pd.read_csv(Path('../data/eagles.csv'))
+
+# >>> df.info()
+# <class 'pandas.core.frame.DataFrame'>
+# RangeIndex: 8 entries, 0 to 7
+# Data columns (total 5 columns):
+#    Column  Non-Null Count  Dtype
+# ---  ------  --------------  -----
+#  0   y       8 non-null      int64
+#  1   n       8 non-null      int64
+#  2   P       8 non-null      object
+#  3   A       8 non-null      object
+#  4   V       8 non-null      object
+# dtypes: int64(2), object(3)
+# memory usage: 452.0 bytes
+
+for col in list('PAV'):
+    df[col] = df[col].astype('category')
+
+with pm.Model() as the_model:
+    P = df['P'] == 'L'
+    V = df['V'] == 'L'
+    A = df['A'] == 'A'
+    α = pm.Normal('α', 0, 10)
+    β_P = pm.Normal('β_P', 0, 5)
+    β_V = pm.Normal('β_V', 0, 5)
+    β_A = pm.Normal('β_A', 0, 5)
+    p = pm.math.invlogit(α + β_P * P + β_V * V + β_A * A)
+    y = pm.Binomial('y', df['n'], p, observed=df['y'])
+    m_quap = sts.quap(data=df)
+    m_ulam = sts.ulam(data=df)
+
+print('quap:')
+sts.precis(m_quap)
+print('ulam:')
+sts.precis(m_ulam)
+
+# =============================================================================
+# =============================================================================
