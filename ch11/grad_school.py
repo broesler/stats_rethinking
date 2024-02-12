d22eebe328b53bfbe934f1ccc77fd7d7a79a9b31
Bernard Roesler <bernard.roesler@gmail.com>
2024-02-12 15:26:52 -0500

    Compute post-stratification gender contrasts.

--- ch11/grad_school.py
+++ ch11/grad_school.py
@@ -10,6 +10,7 @@
 # =============================================================================
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import pymc as pm
 import seaborn as sns
@@ -151,6 +152,9 @@ ax = sts.postcheck(
 ax.set_title('Model by Department and Gender')
 
 
+# -----------------------------------------------------------------------------
+#         Lecture 9 Model and Plots
+# -----------------------------------------------------------------------------
 # Follow Lecture 09 at 1:02:38 to model total and direct effects of gender
 # and department:
 # <https://youtu.be/Zi6N3GLUJmw?si=4sSyaNhENICrtRJE&t=3758>
@@ -183,24 +187,70 @@ diff_pGD = expit(post_GD['α']).diff('α_dim_0').squeeze()
 fig, axs = plt.subplots(num=3, ncols=2, clear=True)
 fig.set_size_inches((10, 4), forward=True)
 
+# Gender only
 axs[0].set(title='Gender Only',
            xlabel='Gender Contrast [probability]')
-axs[1].set(title='Gender and Department',
-           xlabel='Gender Contrast [probability]')
-
-# Gender only
 sns.kdeplot(diff_p.stack(sample=('chain', 'draw')),
             bw_adjust=0.5, c='C3', ax=axs[0])
 axs[0].text(s='men advantaged →', x=0.5, y=0.99,
             ha='center', va='top', transform=axs[0].transAxes)
 
 # Gender and Dept
+axs[1].set(title='Gender and Department',
+           xlabel='Gender Contrast [probability]')
+axs[1].axvline(0, ls='--', c='grey', lw=1, alpha=0.5, zorder=0)
 sns.kdeplot(diff_pGD.stack(sample=('chain', 'draw')).transpose('sample', ...),
             bw_adjust=0.5, ax=axs[1], legend=True)
-axs[1].axvline(0, ls='--', c='grey', lw=1, alpha=0.5, zorder=0)
 axs[1].text(s='← women advantaged       men advantaged →', x=0.0, y=3.95,
             ha='center', va='top')
-axs[1].legend()
+
+
+# -----------------------------------------------------------------------------
+#         Post-stratification Simulation
+# -----------------------------------------------------------------------------
+total_apps = df['applications'].sum()
+apps_per_dept = df.groupby('dept', observed=True)['applications'].sum()
+
+# Simulate if all applications are from women
+eval_at = dict(
+    D=np.repeat(df['dept'].cat.codes.unique(), apps_per_dept),
+    N=np.ones(total_apps).astype(int),
+    G=np.zeros(total_apps).astype(int)  # all women
+)
+
+p_W = sts.lmeval(mGD, out=mGD.model.p, eval_at=eval_at)
+
+# Simulate if all applications are from men
+eval_at['G'] = np.ones(total_apps).astype(int)  # all men
+p_M = sts.lmeval(mGD, out=mGD.model.p, eval_at=eval_at)
+
+# Summarize the contrast
+diff_p_sim = p_M - p_W
+
+fig, axs = plt.subplots(num=4, ncols=2, clear=True)
+fig.set_size_inches((10, 4), forward=True)
+
+ax = axs[0]
+ax.axvline(0, ls='--', c='grey', lw=1, alpha=0.5, zorder=0)
+sns.kdeplot(diff_p_sim.to_numpy().flatten(), bw_adjust=0.5, ax=ax)
+ax.text(s='← women advantaged       men advantaged →', x=0.0, y=8.5,
+        ha='center', va='top')
+ax.set(title='Post-Stratification',
+       xlabel='Effect of Gender Perception')
+
+# Show each dept with weight as in population
+w = df.groupby('dept')['applications'].sum() / total_apps
+
+ax = axs[1]
+ax.set(title='Post-Stratification by Dept.',
+       xlabel='Effect of Gender Perception')
+ax.axvline(0, ls='--', c='grey', lw=1, alpha=0.5, zorder=0)
+sns.kdeplot(diff_pGD.stack(sample=('chain', 'draw')).transpose('sample', ...),
+            bw_adjust=0.5,
+            color=(w.values*np.ones((3, 1))).T,  # FIXME doesn't work??
+            ax=ax)
+ax.text(s='← women advantaged       men advantaged →', x=0.0, y=3.95,
+            ha='center', va='top')
 
 plt.ion()
 plt.show()
