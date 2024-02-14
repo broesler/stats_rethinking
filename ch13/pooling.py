c937b114c7a7bc4081ceb1f6c06e7d001c6bcc23
Bernard Roesler <bernard.roesler@gmail.com>
2024-02-13 22:20:27 -0500

    Update plotting.

--- ch13/pooling.py
+++ ch13/pooling.py
@@ -15,11 +15,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import pymc as pm
-import xarray as xr
 
-from pathlib import Path
 from scipy import stats
-from scipy.special import logit, expit
+from scipy.special import expit
 
 import stats_rethinking as sts
 
@@ -35,55 +33,63 @@ assert len(Ni) == nponds
 a_pond = stats.norm(a_bar, sigma).rvs(nponds)
 
 # (R code 13.9)
-dsim = pd.DataFrame(dict(pond=range(nponds), Ni=Ni, true_a=a_pond))
+df = pd.DataFrame(dict(pond=range(nponds), Ni=Ni, true_a=a_pond))
 
 # Simulate survivors (R code 13.11)
-dsim['Si'] = stats.binom(dsim['Ni'], expit(dsim['true_a'])).rvs(nponds)
+df['Si'] = stats.binom(df['Ni'], expit(df['true_a'])).rvs(nponds)
 
 # Compute no-pooling estimates (R code 13.12)
-dsim['p_nopool'] = dsim['Si'] / dsim['Ni']
+df['p_nopool'] = df['Si'] / df['Ni']
 
 # Compute partial pooling estimates from a model (R code 13.13)
 with pm.Model():
-    N = pm.ConstantData('N', dsim['Ni'])
-    pond = pm.ConstantData('pond', dsim['pond'])
+    N = pm.ConstantData('N', df['Ni'])
+    pond = pm.ConstantData('pond', df['pond'])
     α_bar = pm.Normal('α_bar', 0, 1.5)
     σ = pm.Exponential('σ', 1)
     α_pond = pm.Normal('α_pond', α_bar, σ, shape=pond.shape)
     p = pm.Deterministic('p', pm.math.invlogit(α_pond[pond]))
-    S = pm.Binomial('S', N, p, observed=dsim['Si'])
-    m13_3 = sts.ulam(data=dsim)
+    S = pm.Binomial('S', N, p, observed=df['Si'])
+    m13_3 = sts.ulam(data=df)
 
 # Get the posterior and estimates (R code 13.15)
 post = m13_3.get_samples()
-dsim['p_partpool'] = expit(post['α_pond'].mean(('chain', 'draw')))
+df['p_partpool'] = expit(post['α_pond'].mean(('chain', 'draw')))
 
 # True values (R code 13.16)
-dsim['p_true'] = expit(dsim['true_a'])
+df['p_true'] = expit(df['true_a'])
 
 # Compute the errors
-dsim['nopool_err'] = np.abs(dsim['p_nopool'] - dsim['p_true'])
-dsim['partpool_err'] = np.abs(dsim['p_partpool'] - dsim['p_true'])
+df['nopool_err'] = np.abs(df['p_nopool'] - df['p_true'])
+df['partpool_err'] = np.abs(df['p_partpool'] - df['p_true'])
 
 # Plot the errors
 fig, ax = plt.subplots(num=1, clear=True)
 
 for i, s in zip(range(len(Nis)), ['tiny', 'small', 'medium', 'large']):
-    # TODO axis transform 0.9
-    ax.text(s=f"{s} ponds ({Nis[i]})", x=15*(i+1) - 7.5, y=0.37,
-            ha='center', va='bottom')
+    ax.text(
+        s=f"{s} ponds ({Nis[i]})",
+        x=15*(i+1) - 7.5,
+        y=1.0,
+        ha='center',
+        va='top',
+        transform=ax.get_xaxis_transform()  # x = data, y = axis
+    )
     if i > 0:
-        ax.axvline(i*15 + 0.5, ls='--', c='gray', lw=1)
+        ax.axvline(i*15 - 0.5, ls='--', c='gray', lw=1)
 
-ax.scatter('pond', 'nopool_err', c='C0', alpha=0.6, data=dsim)
-ax.scatter('pond', 'partpool_err', c='C3', alpha=0.6, data=dsim)
+ax.scatter('pond', 'nopool_err', c='C0', alpha=0.6, data=df)
+ax.scatter('pond', 'partpool_err', c='C3', alpha=0.6, data=df)
 
 for n in Nis:
-    df = dsim.loc[dsim['Ni'] == n]
-    ax.plot(df['pond'], df['nopool_err'].mean()*np.ones_like(df), c='C0')
-    ax.plot(df['pond'], df['partpool_err'].mean()*np.ones_like(df), c='C3')
+    tf = df.loc[df['Ni'] == n]
+    ax.plot(tf['pond'], np.full_like(tf, tf['nopool_err'].mean()), c='C0')
+    ax.plot(tf['pond'], np.full_like(tf, tf['partpool_err'].mean()), c='C3')
 
+ax.legend(loc='center right')
 ax.set(xlabel='pond',
        ylabel='absolute error')
+ax.spines[['top', 'right']].set_visible(False)
+
 # =============================================================================
 # =============================================================================
