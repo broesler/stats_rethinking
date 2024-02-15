de5ccd26712d2a453e6f3217f401e72b4249af8c
Bernard Roesler <bernard.roesler@gmail.com>
2024-02-15 15:04:10 -0500

    Plot urban/rural model.

--- ch13/ch13H1.py
+++ ch13/ch13H1.py
@@ -60,7 +60,7 @@ districts = np.arange(1, df['district'].max()+1)
 df['district'] = pd.Categorical(df['district'], categories=districts)
 
 # Make a model of contraception by district
-with pm.Model() as model:
+with pm.Model():
     D = pm.ConstantData('D', df['district'].astype(int) - 1)  # 0-indexed
     α_bar = pm.Normal('α_bar', 0, 1)
     σ = pm.Exponential('σ', 1)
@@ -76,26 +76,57 @@ sts.precis(mCD)
 #         Visualize the Data
 # -----------------------------------------------------------------------------
 counts = df.groupby('district')['woman'].count()
+props = df.groupby('district')['use_contraception'].sum() / counts
 
 fig, ax = plt.subplots(num=1, clear=True)
 ax.bar(districts, counts.values, color='C3', width=0.5)
 ax.set(xlabel='district',
        ylabel='number of women')
 
-# Plot the data and posterior predictions
-props = df.groupby('district')['use_contraception'].sum() / counts
-p_est = expit(mCD.samples['α']).mean(('chain', 'draw'))
-a = (1 - 0.89) / 2
-p_PI = expit(mCD.samples['α']).quantile([a, 1-a], dim=('chain', 'draw'))
 
+def plot_model(model, urban=None, ax=None):
+    """Plot the data and posterior predictions."""
+    if ax is None:
+        ax = plt.gca()
+
+    a = (1 - 0.89) / 2
+
+    if urban is None:
+        # Use all of the data
+        counts = df.groupby('district')['woman'].count()
+        props = df.groupby('district')['use_contraception'].sum() / counts
+        p_samp = expit(model.samples['α'])
+    else:
+        # Filter by urban/rural status
+        tf = df.loc[df['urban'] == urban]
+        counts = tf.groupby('district')['woman'].count()
+        props = tf.groupby('district')['use_contraception'].sum() / counts
+        p_samp = expit(
+            model.samples['α'] 
+            + (model.samples['β'].rename(dict({'β_dim_0': 'α_dim_0'}))
+               * urban)
+        )
+
+    p_est = p_samp.mean(('chain', 'draw'))
+    p_PI = p_samp.quantile([a, 1-a], dim=('chain', 'draw'))
+
+    ax.scatter(districts, props, c='k', alpha=0.8)
+    ax.errorbar(districts, p_est, yerr=np.abs(p_PI - p_est),
+                ls='none', marker='o', lw=3, alpha=0.6,
+                c='C0' if urban else 'C3')
+
+    ax.set(xlabel='district',
+           ylabel='prob. use contraception',
+           ylim=(-0.05, 1.05))
+
+    return ax
+
+
+# Plot the data and model results
 fig, ax = plt.subplots(num=2, clear=True)
 fig.set_size_inches((10, 5), forward=True)
-ax.scatter(districts, props, c='k', alpha=0.8)
-ax.errorbar(districts, p_est, yerr=np.abs(p_PI - p_est),
-            ls='none', marker='o', lw=3, c='C3', alpha=0.6)
-ax.set(xlabel='district',
-       ylabel='prob. use contraception',
-       ylim=(-0.05, 1.05))
+ax.set(title='Proportion by District Only')
+plot_model(mCD, ax=ax)
 
 # Label sample sizes of K largest and smallest proportions
 K = 5
@@ -120,13 +151,40 @@ for d, p, N in zip(hi_d, hi_p, hi_N):
 
 ax.annotate(
     'No data!',
-    xy=(54, p_est[53] + 0.2),
+    xy=(54, 0.7),
     xytext=(0, 50),
     textcoords='offset points',
     ha='center', 
     va='bottom',
     arrowprops=dict(arrowstyle='->', lw=2)
 )
+
+
+# -----------------------------------------------------------------------------
+#         Build a model with urban
+# -----------------------------------------------------------------------------
+with pm.Model():
+    D = pm.ConstantData('D', df['district'].astype(int) - 1)  # 0-indexed
+    U = pm.ConstantData('U', df['urban'].astype(int))
+    α_bar = pm.Normal('α_bar', 0, 1)
+    β_bar = pm.Normal('β_bar', 0, 1)
+    σ = pm.Exponential('σ', 1)
+    τ = pm.Exponential('τ', 1)
+    α = pm.Normal('α', α_bar, σ, shape=districts.shape)
+    β = pm.Normal('β', β_bar, σ, shape=districts.shape)
+    p = pm.Deterministic('p', pm.math.invlogit(α[D] + β[D]*U))
+    C = pm.Bernoulli('C', p, observed=df['use_contraception'])
+    mCDU = sts.ulam(data=df)
+
+
+fig, axs = plt.subplots(num=3, nrows=2, sharex=True, clear=True)
+fig.set_size_inches((10, 8), forward=True)
+
+for u, ax in enumerate(axs):
+    plot_model(mCDU, urban=u, ax=ax)
+    ax.set_title('Urban' if u else 'Rural')
+
+axs[0].set_xlabel('')
 
 # =============================================================================
 # =============================================================================
